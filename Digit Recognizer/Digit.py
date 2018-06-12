dd798afc7b1146fd6ba6558591e0aa25b2844fa3
r04921114 <r04921114@ntu.edu.tw>
2018-06-12 13:37:41 +0800

    Add files via upload

--- Digit Recognizer/Digit.py
+++ Digit Recognizer/Digit.py
@@ -9,8 +9,8 @@ from keras.layers import Convolution2D as Conv2D
 from keras.utils import to_categorical
 
 num_classes = 10
-batch_size = 210
-epochs = 3
+batch_size = 32
+epochs = 10
 
 # load data
 x_train = pd.read_csv('train.csv')
@@ -37,14 +37,15 @@ model.add(Conv2D(32,(3,3),padding='same',activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
 
-model.add(Conv2D(128,(3,3),padding='same',activation='relu'))
+model.add(Conv2D(64,(3,3),padding='same',activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(64,(3,3),padding='same',activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
 
 model.add(Flatten())
 model.add(Dense(512,activation='relu'))
 model.add(Dropout(0.5))
-
 model.add(Dense(num_classes)) #output :10 dim
 model.add(Activation('softmax'))
 
@@ -59,7 +60,7 @@ datagen = ImageDataGenerator(
 		featurewise_std_normalization=False,  # divide inputs by std of the dataset
 		samplewise_std_normalization=False,  # divide each input by its std
 		zca_whitening=False,  # apply ZCA whitening
-		rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+		rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
 		width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
 		height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
 		horizontal_flip=False,  # randomly flip images
@@ -73,3 +74,10 @@ model.fit_generator(datagen.flow(X1, Y1,
                         steps_per_epoch=X1.shape[0],
                         validation_data=(X2, Y2))
 scores = model.evaluate(X2, Y2, verbose=1)
+
+Y_test = model.predict(test,batch_size=batch_size)
+
+with open(result.csv,'w') as f:
+	f.write('imageId,Label\n')
+	for i in range(len(Y_test)):
+		f.write("".join([str(i+1),',',str(Y_test[i]),'\n']))
